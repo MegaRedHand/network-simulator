a159f01eee1ab9848061824959757ace75f79ccf
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-10-05 17:05:44 -0300

    chore: remove bunny

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,15 @@
-import { Application, Sprite, Assets, Graphics, GraphicsContext, FederatedPointerEvent } from 'pixi.js';
-import Bunny from './bunny.png';
+import { Application, Graphics, GraphicsContext, FederatedPointerEvent } from 'pixi.js';
 import './style.css';
 
 
 // IIFE to avoid errors
 (async () => {
-    const button = document.createElement("button");
-    button.textContent = "Open file";
-    let fileContent = null;
-
-    const input = document.createElement('input');
-    input.type = 'file';
-
-    button.onclick = () => {
-        input.click();
-    }
-    document.body.appendChild(button);
-
-    // The application will create a renderer using WebGL, if possible,
-    // with a fallback to a canvas render. It will also setup the ticker
-    // and the root stage PIXI.Container
+    // Initialization
     const app = new Application();
 
-    // Wait for the Renderer to be available
     await app.init({ width: window.innerWidth, height: window.innerHeight, resolution: devicePixelRatio });
 
-    // The application will create a canvas element for you that you
-    // can then insert into the DOM
     document.body.appendChild(app.canvas);
-
-    await Assets.load(Bunny);
-
-    const bunny = Sprite.from(Bunny);
-
-    const resizeBunny = () => {
-        // Setup the position of the bunny
-        bunny.x = app.renderer.width / 5;
-        bunny.y = app.renderer.height / 5;
-        bunny.width = app.renderer.width / 5;
-        bunny.height = app.renderer.height / 5;
-    }
-
-    resizeBunny();
-
-    // Rotate around the center
-    bunny.anchor.x = 0.5;
-    bunny.anchor.y = 0.5;
 
     let rect = new Graphics()
         .rect(0, 0, app.renderer.width, app.renderer.height)
@@ -56,9 +20,11 @@ import './style.css';
         rect.height = app.renderer.height;
     }
 
-    // Add the objects to the scene we are building
     app.stage.addChild(rect);
-    app.stage.addChild(bunny);
+    resizeRect();
+
+
+    // Circle and lines logic
 
     const circleContext = new GraphicsContext().circle(0, 0, 10).fill(0xff0000);
 
@@ -96,35 +62,18 @@ import './style.css';
 
     rect.eventMode = 'static';
 
-    // Listen for frame updates
-    app.ticker.add(() => {
-        // each frame we spin the bunny around a bit
-        bunny.rotation += 0.005;
-    });
+
+    // Ticker logic
+
+    app.ticker.add(() => { });
+
+
+    // Resize logic
 
     function resize() {
-        // Resize the renderer
         app.renderer.resize(window.innerWidth, window.innerHeight);
-
-        resizeBunny();
         resizeRect();
     }
 
     window.addEventListener('resize', resize);
-
-    input.onchange = () => {
-        const file = input.files[0];
-
-        console.log(file);
-        // setting up the reader
-        const reader = new FileReader();
-        reader.readAsDataURL(file); // this is reading as data url
-
-        // here we tell the reader what to do when it's done reading...
-        reader.onload = async (readerEvent) => {
-            fileContent = readerEvent.target.result; // this is the content!
-            const txt = await Assets.load(fileContent);
-            bunny.texture = txt;
-        }
-    }
 })();
