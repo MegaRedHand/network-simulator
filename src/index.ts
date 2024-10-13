e3089a579aad369365bbd069c672f02275a46a34
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-10-13 16:32:21 -0300

    feat: use strategies instead of enum

--- src/index.ts
+++ src/index.ts
@@ -1,105 +1,88 @@
 // Doing this includes the file in the build
 import './style.css';
 
-import { Application, Graphics, GraphicsContext, FederatedPointerEvent, EventSystem, PointData } from 'pixi.js';
+// Assets
 import RouterSvg from './assets/router.svg';
 import ConnectionSvg from './assets/connection.svg';
+
+import { Application, Graphics, GraphicsContext, FederatedPointerEvent, Container, EventSystem, PointData } from 'pixi.js';
+
 import * as pixi_viewport from 'pixi-viewport';
 
 
 const WORLD_WIDTH = 10000;
 const WORLD_HEIGHT = 10000;
 
-
-enum CursorMode {
-    Router,
-    Connection,
-}
-
+// > context.ts
 
 class GlobalContext {
-    // TODO: merge mode and selected fields into strategy class
-    mode: CursorMode = CursorMode.Router;
-    selected: Circle = null;
+    mode: ModeStrategy = new MoveMode();
 
     viewport: Viewport = null;
-
-    popSelected() {
-        const selected = this.selected;
-        this.selected = null;
-        return selected;
-    }
 
     setViewport(viewport: Viewport) {
         this.viewport = viewport;
     }
 }
 
-class Circle extends Graphics {
-    static graphicsContext = new GraphicsContext().circle(0, 0, 10).fill(0xff0000);
+interface ModeStrategy {
+    initialize(ctx: GlobalContext): void;
+    clickViewport(ctx: GlobalContext, e: FederatedPointerEvent): void;
+    clickCircle(ctx: GlobalContext, e: FederatedPointerEvent, circle: Circle): void;
+}
 
-    constructor(ctx: GlobalContext, position: PointData) {
-        super(Circle.graphicsContext);
-        this.position = position;
-        this.zIndex = 2;
-        this.on('click', (e) => this.onClick(ctx, e));
-        this.eventMode = 'static';
+class MoveMode implements ModeStrategy {
+    initialize(ctx: GlobalContext) { }
+    clickViewport(ctx: GlobalContext, e: FederatedPointerEvent) { }
+    clickCircle(ctx: GlobalContext, e: FederatedPointerEvent, circle: Circle) { }
+}
+
+class RouterMode {
+    initialize(ctx: GlobalContext) { }
+    clickViewport(ctx: GlobalContext, e: FederatedPointerEvent) {
+        const position = ctx.viewport.toWorld(e.global);
+        const circle = new Circle(ctx, position);
+        ctx.viewport.addChild(circle);
     }
-
-    onClick(ctx: GlobalContext, e: FederatedPointerEvent) {
-        if (ctx.mode != CursorMode.Connection) {
-            return;
-        }
+    clickCircle(ctx: GlobalContext, e: FederatedPointerEvent, circle: Circle) {
+        // To avoid overlapping circles
         e.stopPropagation();
-        const selected = ctx.popSelected();
+    }
+}
+
+class ConnectionMode {
+    private selected: Circle = null;
+
+    initialize(ctx: GlobalContext) { }
+    clickViewport(ctx: GlobalContext, e: FederatedPointerEvent) {
+        e.stopPropagation();
+    }
+    clickCircle(ctx: GlobalContext, e: FederatedPointerEvent, circle: Circle) {
+        e.stopPropagation();
+        const selected = this.popSelected();
 
         if (selected === null) {
-            ctx.selected = this;
-        } else {
-            // TODO: this could be moved to a separate function/class
-            const line = new Graphics()
-                .moveTo(selected.x, selected.y)
-                .lineTo(this.x, this.y)
-                .stroke({ width: 2, color: 0 });
-            line.zIndex = 1;
-            ctx.viewport.addChild(line);
+            this.selected = circle;
+            return;
         }
+        // TODO: this could be moved to a separate function/class
+        const line = new Graphics()
+            .moveTo(selected.x, selected.y)
+            .lineTo(circle.x, circle.y)
+            .stroke({ width: 2, color: 0 });
+        line.zIndex = 1;
+        ctx.viewport.addChild(line);
+    }
+
+    // Private
+    private popSelected() {
+        const selected = this.selected;
+        this.selected = null;
+        return selected;
     }
 }
 
-class LeftBar {
-    private leftBar: HTMLElement;
-
-    constructor(leftBar: HTMLElement) {
-        this.leftBar = leftBar;
-    }
-    static getFrom(document: Document) {
-        return new LeftBar(document.getElementById('left-bar'));
-    }
-
-    addButton(src: string, onClick: () => void) {
-        const button = document.createElement("button");
-        button.classList.add("tool-button");
-
-        button.onclick = onClick;
-        this.leftBar.appendChild(button);
-
-        const img = document.createElement("img");
-        img.src = src;
-        button.appendChild(img);
-    }
-}
-
-class RightBar {
-    private rightBar: HTMLElement;
-
-    constructor(rightBar: HTMLElement) {
-        this.rightBar = rightBar;
-    }
-    static getFrom(document: Document) {
-        return new RightBar(document.getElementById('right-bar'));
-    }
-}
+// > graphics.ts
 
 class Background extends Graphics {
     constructor() {
@@ -132,15 +115,61 @@ class Viewport extends pixi_viewport.Viewport {
         this.addChild(new Background());
 
         // Circle and lines logic
-        this.on('click', (e) => {
-            if (ctx.mode == CursorMode.Router) {
-                const position = this.toWorld(e.global);
-                const circle = new Circle(ctx, position);
-                this.addChild(circle);
-            }
-        });
+        this.on('click', (e) => { ctx.mode.clickViewport(ctx, e) });
     }
 }
+
+class Circle extends Graphics {
+    static graphicsContext = new GraphicsContext().circle(0, 0, 10).fill(0xff0000);
+
+    constructor(ctx: GlobalContext, position: PointData) {
+        super(Circle.graphicsContext);
+        this.position = position;
+        this.zIndex = 2;
+        this.on('click', (e) => ctx.mode.clickCircle(ctx, e, this));
+        this.eventMode = 'static';
+    }
+}
+
+// > left_bar.ts
+
+class LeftBar {
+    private leftBar: HTMLElement;
+
+    constructor(leftBar: HTMLElement) {
+        this.leftBar = leftBar;
+    }
+    static getFrom(document: Document) {
+        return new LeftBar(document.getElementById('left-bar'));
+    }
+
+    addButton(src: string, onClick: () => void) {
+        const button = document.createElement("button");
+        button.classList.add("tool-button");
+
+        button.onclick = onClick;
+        this.leftBar.appendChild(button);
+
+        const img = document.createElement("img");
+        img.src = src;
+        button.appendChild(img);
+    }
+}
+
+// > right_bar.ts
+
+class RightBar {
+    private rightBar: HTMLElement;
+
+    constructor(rightBar: HTMLElement) {
+        this.rightBar = rightBar;
+    }
+    static getFrom(document: Document) {
+        return new RightBar(document.getElementById('right-bar'));
+    }
+}
+
+// > index.ts
 
 // IIFE to avoid errors
 (async () => {
@@ -161,15 +190,14 @@ class Viewport extends pixi_viewport.Viewport {
     // Left bar logic
     const leftBar = LeftBar.getFrom(document);
 
+    // Add move button
+    leftBar.addButton(RouterSvg, () => { ctx.mode = new MoveMode() });
+
     // Add router button
-    leftBar.addButton(RouterSvg, () => {
-        ctx.mode = CursorMode.Router;
-    });
+    leftBar.addButton(RouterSvg, () => { ctx.mode = new RouterMode() });
 
     // Add connection button
-    leftBar.addButton(ConnectionSvg, () => {
-        ctx.mode = CursorMode.Connection;
-    });
+    leftBar.addButton(ConnectionSvg, () => { ctx.mode = new ConnectionMode() });
 
     // Get right bar
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
