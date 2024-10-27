84a4001caeaa81cc86ca1a77449e43b7b97a7719
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-10-27 15:30:04 -0300

    chore: format

--- test/ip.test.ts
+++ test/ip.test.ts
@@ -1,13 +1,12 @@
-import * as ip from '../src/packets/ip';
+import * as ip from "../src/packets/ip";
 
-
-describe('IP module', () => {
-    test('computing checksum results in valid checksum', () => {
-        const packet = new ip.IPv4Packet(
-            new ip.IpAddress(new Uint8Array([192, 168, 1, 1])),
-            new ip.IpAddress(new Uint8Array([192, 168, 1, 2])),
-            new ip.EmptyPayload(),
-        )
-        expect(packet.validateChecksum()).toBe(true);
-    });
+describe("IP module", () => {
+  test("computing checksum results in valid checksum", () => {
+    const packet = new ip.IPv4Packet(
+      new ip.IpAddress(new Uint8Array([192, 168, 1, 1])),
+      new ip.IpAddress(new Uint8Array([192, 168, 1, 2])),
+      new ip.EmptyPayload(),
+    );
+    expect(packet.validateChecksum()).toBe(true);
+  });
 });
