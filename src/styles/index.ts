cb8f762b395fcef203c9414814cd4bdf53c53331
pgallino <pgallino@fi.uba.ar>
2024-11-23 22:53:31 -0300

    Refactor: CSS Modularization and Reordering Refactor

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,1 +1,8 @@
-import "./style.css";
+import './global.css';
+import './top-bar.css';
+import './left-bar.css';
+import './canvas.css';
+import './right-bar.css';
+import './right-bar-buttons.css';
+import './table.css';
+import './buttons.css';
