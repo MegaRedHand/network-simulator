cc4f8fc94a7c6f988d1a039e092ff0fd85b96898
pgallino <pgallino@fi.uba.ar>
2024-11-23 22:54:43 -0300

    chore: fix lint errors

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,8 +1,8 @@
-import './global.css';
-import './top-bar.css';
-import './left-bar.css';
-import './canvas.css';
-import './right-bar.css';
-import './right-bar-buttons.css';
-import './table.css';
-import './buttons.css';
+import "./global.css";
+import "./top-bar.css";
+import "./left-bar.css";
+import "./canvas.css";
+import "./right-bar.css";
+import "./right-bar-buttons.css";
+import "./table.css";
+import "./buttons.css";
