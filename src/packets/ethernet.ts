d7dd17185e9965a1e267699e4a9c822dd1d6ead9
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-01-11 17:43:02 -0300

    docs: rename some fields and add more docs

--- src/packets/ethernet.ts
+++ src/packets/ethernet.ts
@@ -1,3 +1,8 @@
+// From https://en.wikipedia.org/wiki/EtherType
+export const IP_PROTOCOL_TYPE = 0x0800;
+export const ARP_PROTOCOL_TYPE = 0x0806;
+export const IPV6_PROTOCOL_NUMBER = 0x86dd;
+
 /// Medium Access Control (MAC) address
 export class MacAddress {
   // 6 bytes
@@ -37,20 +42,29 @@ export class MacAddress {
 }
 
 export class EthernetFrame {
-  // Info taken from wikipedia/wireshark wiki
+  // Info taken from Computer Networking: A Top-Down Approach
+  // 8 bytes
   // 7 bytes preamble and 1 byte start of frame delimiter
+  /// Used to synchronize the communication
   readonly preamble = new Uint8Array([
     0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010,
     0b10101010, 0b10101011,
   ]);
   // 6 bytes
+  // Destination MAC address
   destination: MacAddress;
   // 6 bytes
+  // Source MAC address
   source: MacAddress;
   // 2 bytes
-  length: number;
+  // The payload's type
+  type: number;
   // 46-1500 bytes
-  userData: Uint8Array;
+  // If the payload is smaller than 46 bytes, it is padded.
+  // TODO: make this an interface
+  // The payload
+  payload: Uint8Array;
   // 4 bytes
-  fcs: number;
+  // Cyclic Redundancy Check (CRC)
+  crc: number;
 }
