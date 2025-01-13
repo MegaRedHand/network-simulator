c034aa2af7e630776b60e2277ec18040096f0a32
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-01-12 21:44:16 -0300

    add TODO

--- test/tcp.test.ts
+++ test/tcp.test.ts
@@ -13,15 +13,8 @@ describe("TCP module", () => {
     );
   });
 
-  test("constructor works", () => {
-    new TcpSegment(0, 0, 0, 0, new Flags(), Uint8Array.of());
-    new TcpSegment(
-      0xffff,
-      0xffff,
-      0xffffffff,
-      0xffffffff,
-      new Flags(),
-      Uint8Array.of(),
-    );
+  test("toBytes works", () => {
+    const segment = new TcpSegment(0, 0, 0, 0, new Flags(), Uint8Array.of());
+    // TODO: use wireshark to get some examples
   });
 });
