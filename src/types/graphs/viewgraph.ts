650a5d214691fe3960ec252ab10938a4072138bb
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-01-31 10:22:53 -0300

    fix: recompute connections on datagraph when device is re-added (#109)
    Depends on #98

--- src/types/graphs/viewgraph.ts
+++ src/types/graphs/viewgraph.ts
@@ -36,42 +36,56 @@ export class ViewGraph {
     this.datagraph.getDevices().forEach((graphNode, deviceId) => {
       if (layerIncluded(layerFromType(graphNode.type), this.layer)) {
         const deviceInfo = { id: deviceId, node: graphNode };
-        this.addDevice(deviceInfo);
+        this.createDevice(deviceInfo);
 
-        this.layer_dfs(
-          this.datagraph.getDevices(),
-          deviceId,
-          deviceId,
-          new Set([deviceId]),
-          connections,
-        );
+        this.computeLayerConnections(deviceId, connections);
       }
     });
 
+    this.addConnections(connections);
+    console.log("Finished constructing ViewGraph");
+  }
+
+  addDevice(deviceData: CreateDevice) {
+    const device = this.createDevice(deviceData);
+    if (deviceData.node.connections.size !== 0) {
+      const connections = new Set<string>();
+      this.computeLayerConnections(deviceData.id, connections);
+
+      this.addConnections(connections);
+    }
+    return device;
+  }
+
+  // Add a device to the graph
+  private createDevice(deviceData: CreateDevice): Device {
+    if (this.devices.has(deviceData.id)) {
+      console.warn(
+        `Device with ID ${deviceData.id} already exists in the graph.`,
+      );
+      return this.devices.get(deviceData.id);
+    }
+    const device = createDevice(deviceData, this);
+
+    this.devices.set(device.id, device);
+    this.viewport.addChild(device);
+    console.log(`Device added with ID ${device.id}`);
+    return this.devices.get(deviceData.id);
+  }
+
+  private addConnections(connections: Set<string>) {
     connections.forEach((key) => {
       const connection = parseConnectionKey(key);
       const device1 = this.getDevice(connection.id1);
       const device2 = this.getDevice(connection.id2);
+      if (!(device1 && device2)) {
+        console.warn("At least one device in connection does not exist");
+        return;
+      }
       this.drawEdge(device1, device2);
       device1.addConnection(device2.id);
       device2.addConnection(device1.id);
     });
-    console.log("Finished constructing ViewGraph");
-  }
-
-  // Add a device to the graph
-  addDevice(deviceData: CreateDevice): Device {
-    if (!this.devices.has(deviceData.id)) {
-      const device = createDevice(deviceData, this);
-      this.devices.set(device.id, device);
-      this.viewport.addChild(device);
-      console.log(`Device added with ID ${device.id}`);
-    } else {
-      console.warn(
-        `Device with ID ${deviceData.id} already exists in the graph.`,
-      );
-    }
-    return this.devices.get(deviceData.id);
   }
 
   drawEdge(device1: Device, device2: Device): Edge {
@@ -331,6 +345,16 @@ export class ViewGraph {
       current = this.devices.get(parentId);
     }
     return path.reverse();
+  }
+
+  private computeLayerConnections(source: DeviceId, connections: Set<string>) {
+    this.layer_dfs(
+      this.datagraph.getDevices(),
+      source,
+      source,
+      new Set([source]),
+      connections,
+    );
   }
 
   private layer_dfs(
