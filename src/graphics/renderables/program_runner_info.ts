f1eaaff42070379e2a3a2d8c9cac0302979bd851
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-02-15 14:59:53 -0300

    feat: refresh program list without refreshing select

--- src/graphics/renderables/program_runner_info.ts
+++ src/graphics/renderables/program_runner_info.ts
@@ -13,74 +13,85 @@ export class ProgramRunnerInfo implements Renderable {
 
   private inputFields: Node[] = [];
 
-  private programInputs: HTMLElement;
+  private runningProgramsTable: HTMLTableElement;
 
   constructor(runner: ProgramRunner, programInfos: ProgramInfo[]) {
     this.runner = runner;
 
     this.addPrograms(programInfos);
+    this.addRunningProgramsList(runner);
   }
 
-  addPrograms(programs: ProgramInfo[]) {
+  private addPrograms(programs: ProgramInfo[]) {
     const programOptions = programs.map(({ name }, i) => {
       return { value: i.toString(), text: name };
     });
-    this.programInputs = document.createElement("div");
+    const programInputs = document.createElement("div");
     let selectedProgram = programs[0];
-    this.programInputs.replaceChildren(...selectedProgram.toHTML());
+    programInputs.replaceChildren(...selectedProgram.toHTML());
     this.inputFields.push(
       // Dropdown for selecting program
       createDropdown("Program", programOptions, "program-selector", (v) => {
         selectedProgram = programs[parseInt(v)];
-        this.programInputs.replaceChildren(...selectedProgram.toHTML());
+        programInputs.replaceChildren(...selectedProgram.toHTML());
       }),
-      this.programInputs,
+      programInputs,
       // Button to run program
       createRightBarButton("Start program", () => {
         const { name } = selectedProgram;
         console.log("Started program: ", name);
         const inputs = selectedProgram.getInputValues();
         this.runner.addRunningProgram(name, inputs);
-        refreshElement();
+        this.refreshTable();
       }),
     );
   }
 
-  addRunningProgramsList(
-    runner: ProgramRunner,
-    runningPrograms: RunningProgram[],
-  ) {
+  private addRunningProgramsList(runner: ProgramRunner) {
+    const runningPrograms = runner.getRunningPrograms();
     if (runningPrograms.length === 0) {
       return;
     }
-    const table = createProgramsTable(runner, runningPrograms);
+    const table = this.createProgramsTable(runner, runningPrograms);
+    this.runningProgramsTable = table;
     this.inputFields.push(table);
+  }
+
+  private createProgramsTable(
+    runner: ProgramRunner,
+    runningPrograms: RunningProgram[],
+  ) {
+    const onDelete = (row: number) => {
+      const { pid } = runningPrograms[row];
+      runner.removeRunningProgram(pid);
+      this.refreshTable();
+      return true;
+    };
+    const rows = runningPrograms.map((program) => [
+      program.pid.toString(),
+      program.name,
+      JSON.stringify(program.inputs),
+    ]);
+    const headers = ["PID", "Name", "Inputs"];
+    // TODO: make table editable?
+    const table = createTable(headers, rows, { onDelete });
+    table.id = "running-programs-table";
+    table.classList.add("right-bar-table");
+    return table;
+  }
+
+  private refreshTable() {
+    const runningPrograms = this.runner.getRunningPrograms();
+    let newTable;
+    if (runningPrograms.length === 0) {
+      newTable = document.createElement("table");
+    } else {
+      newTable = this.createProgramsTable(this.runner, runningPrograms);
+    }
+    this.runningProgramsTable.replaceWith(newTable);
   }
 
   toHTML() {
     return this.inputFields;
   }
-}
-
-function createProgramsTable(
-  runner: ProgramRunner,
-  runningPrograms: RunningProgram[],
-) {
-  const onDelete = (row: number) => {
-    const { pid } = runningPrograms[row];
-    runner.removeRunningProgram(pid);
-    refreshElement();
-    return true;
-  };
-  const rows = runningPrograms.map((program) => [
-    program.pid.toString(),
-    program.name,
-    JSON.stringify(program.inputs),
-  ]);
-  const headers = ["PID", "Name", "Inputs"];
-  // TODO: make table editable?
-  const table = createTable(headers, rows, { onDelete });
-  table.id = "running-programs-table";
-  table.classList.add("right-bar-table");
-  return table;
 }
