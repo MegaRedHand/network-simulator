1a7c31c2d6acf14b2f9c1766b0f25afb11ef1878
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-03-16 19:27:37 -0300

    Revert "refactor: remove AddRemoveDeviceMove superclass"
    This reverts commit 8730cc97bdcde3b58223f08846140c7c321556c6.

--- src/types/undo-redo/moves/addRemoveDevice.ts
+++ src/types/undo-redo/moves/addRemoveDevice.ts
@@ -10,75 +10,69 @@ export interface DeviceData {
   connections?: DeviceId[];
 }
 
-function addDevice(viewgraph: ViewGraph, data: DeviceData) {
-  const datagraph = viewgraph.getDataGraph();
+// Superclass for AddDeviceMove and RemoveDeviceMove
+export abstract class AddRemoveDeviceMove extends BaseMove {
+  data: DeviceData;
 
-  // If ID was unspecified, it means it's a new device
-  if (data.id == undefined) {
-    const id = datagraph.addNewDevice(data.node);
-    data.id = id;
-    data.connections = [];
-  } else {
-    // Add the device to the datagraph and the viewgraph
-    const deviceInfo = structuredClone(data.node);
-    // Clone array to avoid modifying the original
-    const connections = Array.from(data.connections);
-
-    datagraph.addDevice(data.id, deviceInfo, connections);
-    datagraph.regenerateAllRoutingTables();
+  constructor(layer: Layer, data: DeviceData) {
+    // NOTE: we have to deep-copy the data to stop the data from
+    // being modified by the original
+    super(layer);
+    this.data = structuredClone(data);
   }
-  this.adjustLayer(viewgraph);
-  viewgraph.loadDevice(data.id);
-  selectElement(viewgraph.getDevice(data.id));
-  return true;
-}
 
-function removeDevice(viewgraph: ViewGraph, data: DeviceData) {
-  this.adjustLayer(viewgraph);
-  const device = viewgraph.getDevice(data.id);
-  if (device == undefined) {
-    return false;
+  addDevice(viewgraph: ViewGraph) {
+    const datagraph = viewgraph.getDataGraph();
+
+    // If ID was unspecified, it means it's a new device
+    if (this.data.id == undefined) {
+      const id = datagraph.addNewDevice(this.data.node);
+      this.data.id = id;
+      this.data.connections = [];
+    } else {
+      // Add the device to the datagraph and the viewgraph
+      const deviceInfo = structuredClone(this.data.node);
+      // Clone array to avoid modifying the original
+      const connections = Array.from(this.data.connections);
+
+      datagraph.addDevice(this.data.id, deviceInfo, connections);
+      datagraph.regenerateAllRoutingTables();
+    }
+    this.adjustLayer(viewgraph);
+    viewgraph.loadDevice(this.data.id);
+    selectElement(viewgraph.getDevice(this.data.id));
+    return true;
   }
-  // This also deselects the element
-  device.delete();
-  return true;
+
+  removeDevice(viewgraph: ViewGraph) {
+    this.adjustLayer(viewgraph);
+    const device = viewgraph.getDevice(this.data.id);
+    if (device == undefined) {
+      return false;
+    }
+    // This also deselects the element
+    device.delete();
+    return true;
+  }
 }
 
 // "Move" is here because it conflicts with AddDevice from viewportManager
-export class AddDeviceMove extends BaseMove {
-  data: DeviceData;
-
-  constructor(layer: Layer, data: DeviceData) {
-    // NOTE: we have to deep-copy the data to stop the data from
-    // being modified by the original
-    super(layer);
-    this.data = structuredClone(data);
-  }
-
+export class AddDeviceMove extends AddRemoveDeviceMove {
   undo(viewgraph: ViewGraph): boolean {
-    return removeDevice(viewgraph, this.data);
+    return this.removeDevice(viewgraph);
   }
 
   redo(viewgraph: ViewGraph): boolean {
-    return addDevice(viewgraph, this.data);
+    return this.addDevice(viewgraph);
   }
 }
 
-export class RemoveDeviceMove extends BaseMove {
-  data: DeviceData;
-
-  constructor(layer: Layer, data: DeviceData) {
-    // NOTE: we have to deep-copy the data to stop the data from
-    // being modified by the original
-    super(layer);
-    this.data = structuredClone(data);
-  }
-
+export class RemoveDeviceMove extends AddRemoveDeviceMove {
   undo(viewgraph: ViewGraph): boolean {
-    return addDevice(viewgraph, this.data);
+    return this.addDevice(viewgraph);
   }
 
   redo(viewgraph: ViewGraph): boolean {
-    return removeDevice(viewgraph, this.data);
+    return this.removeDevice(viewgraph);
   }
 }
