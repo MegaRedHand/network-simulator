8730cc97bdcde3b58223f08846140c7c321556c6
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-03-16 16:29:58 -0300

    refactor: remove AddRemoveDeviceMove superclass

--- src/types/undo-redo/moves/addRemoveDevice.ts
+++ src/types/undo-redo/moves/addRemoveDevice.ts
@@ -10,8 +10,42 @@ export interface DeviceData {
   connections?: DeviceId[];
 }
 
-// Superclass for AddDeviceMove and RemoveDeviceMove
-export abstract class AddRemoveDeviceMove extends BaseMove {
+function addDevice(viewgraph: ViewGraph, data: DeviceData) {
+  const datagraph = viewgraph.getDataGraph();
+
+  // If ID was unspecified, it means it's a new device
+  if (data.id == undefined) {
+    const id = datagraph.addNewDevice(data.node);
+    data.id = id;
+    data.connections = [];
+  } else {
+    // Add the device to the datagraph and the viewgraph
+    const deviceInfo = structuredClone(data.node);
+    // Clone array to avoid modifying the original
+    const connections = Array.from(data.connections);
+
+    datagraph.addDevice(data.id, deviceInfo, connections);
+    datagraph.regenerateAllRoutingTables();
+  }
+  this.adjustLayer(viewgraph);
+  viewgraph.loadDevice(data.id);
+  selectElement(viewgraph.getDevice(data.id));
+  return true;
+}
+
+function removeDevice(viewgraph: ViewGraph, data: DeviceData) {
+  this.adjustLayer(viewgraph);
+  const device = viewgraph.getDevice(data.id);
+  if (device == undefined) {
+    return false;
+  }
+  // This also deselects the element
+  device.delete();
+  return true;
+}
+
+// "Move" is here because it conflicts with AddDevice from viewportManager
+export class AddDeviceMove extends BaseMove {
   data: DeviceData;
 
   constructor(layer: Layer, data: DeviceData) {
@@ -21,58 +55,30 @@ export abstract class AddRemoveDeviceMove extends BaseMove {
     this.data = structuredClone(data);
   }
 
-  addDevice(viewgraph: ViewGraph) {
-    const datagraph = viewgraph.getDataGraph();
-
-    // If ID was unspecified, it means it's a new device
-    if (this.data.id == undefined) {
-      const id = datagraph.addNewDevice(this.data.node);
-      this.data.id = id;
-      this.data.connections = [];
-    } else {
-      // Add the device to the datagraph and the viewgraph
-      const deviceInfo = structuredClone(this.data.node);
-      // Clone array to avoid modifying the original
-      const connections = Array.from(this.data.connections);
-
-      datagraph.addDevice(this.data.id, deviceInfo, connections);
-      datagraph.regenerateAllRoutingTables();
-    }
-    this.adjustLayer(viewgraph);
-    viewgraph.loadDevice(this.data.id);
-    selectElement(viewgraph.getDevice(this.data.id));
-    return true;
-  }
-
-  removeDevice(viewgraph: ViewGraph) {
-    this.adjustLayer(viewgraph);
-    const device = viewgraph.getDevice(this.data.id);
-    if (device == undefined) {
-      return false;
-    }
-    // This also deselects the element
-    device.delete();
-    return true;
-  }
-}
-
-// "Move" is here because it conflicts with AddDevice from viewportManager
-export class AddDeviceMove extends AddRemoveDeviceMove {
   undo(viewgraph: ViewGraph): boolean {
-    return this.removeDevice(viewgraph);
+    return removeDevice(viewgraph, this.data);
   }
 
   redo(viewgraph: ViewGraph): boolean {
-    return this.addDevice(viewgraph);
+    return addDevice(viewgraph, this.data);
   }
 }
 
-export class RemoveDeviceMove extends AddRemoveDeviceMove {
+export class RemoveDeviceMove extends BaseMove {
+  data: DeviceData;
+
+  constructor(layer: Layer, data: DeviceData) {
+    // NOTE: we have to deep-copy the data to stop the data from
+    // being modified by the original
+    super(layer);
+    this.data = structuredClone(data);
+  }
+
   undo(viewgraph: ViewGraph): boolean {
-    return this.addDevice(viewgraph);
+    return addDevice(viewgraph, this.data);
   }
 
   redo(viewgraph: ViewGraph): boolean {
-    return this.removeDevice(viewgraph);
+    return removeDevice(viewgraph, this.data);
   }
 }
