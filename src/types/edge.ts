b3f8356a0c93fb9336d69ca6037ac0e448f2ca23
Valentín Santander <71946855+V-SANT@users.noreply.github.com>
2025-03-30 19:25:45 -0300

    Improve: Make unselected edges transparent (#180)
    This PR tries to unclutter the view of the canvas by making some edges
    transparent while selecting devices or edges.
    
    - If the user selects a device, all the edges that are not connected to
    the device become transparent.
    
    - If the user selects an edge, all the edges become transparent except
    for the selected one.
    
    - If no devices or edges are selected, none of the edges are transparent
    
    Maybe we can add a setting to add/remove this functionality, keeping it
    on by default. Some users might want the edges to be at full opacity all
    the time.
    
    closes #165

--- src/types/edge.ts
+++ src/types/edge.ts
@@ -87,10 +87,15 @@ export class Edge extends Graphics {
 
   highlight() {
     this.drawEdge(this.startPos, this.endPos, Colors.Violet);
+    this.viewgraph.transparentEdgesForEdge(
+      this.connectedNodes.n1,
+      this.connectedNodes.n2,
+    );
   }
 
   removeHighlight() {
     this.drawEdge(this.startPos, this.endPos, Colors.Lightblue);
+    this.viewgraph.untransparentEdges();
   }
 
   // Method to show the Edge information
@@ -149,6 +154,14 @@ export class Edge extends Graphics {
   destroy() {
     deselectElement();
     super.destroy();
+  }
+
+  becomeTransparent() {
+    this.alpha = 0.2;
+  }
+
+  becomeOpaque() {
+    this.alpha = 1;
   }
 
   public updatePosition(device1: Device, device2: Device) {
