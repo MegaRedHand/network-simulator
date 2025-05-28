4bff4e38dffb8d5d09d35787a46efc9bd4c2fd56
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-05-28 13:54:02 -0300

    Update router_constants.ts

--- src/utils/constants/router_constants.ts
+++ src/utils/constants/router_constants.ts
@@ -1,5 +1,5 @@
 // Router Constants
 export const ROUTER_CONSTANTS = {
-  PACKET_QUEUE_MAX_SIZE: 1024,
-  PROCESSING_SPEED: 8,
+  PACKET_QUEUE_MAX_SIZE: 4096,
+  PROCESSING_SPEED: 1024,
 } as const;
